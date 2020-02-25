252080c94157f48e479b3bea4b41bcf9db484a36
Ridwan Balogun <balogunridwan@gmail.com>
2020-02-25 18:11:25 +0100

    Add Filter for Tags and Captions

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   state={
     tags: [],
     captions: [],
-    searchField: '' 
+    searchTagField: '',
+    searchCaptionField: '' 
   }
 
 
@@ -27,26 +28,32 @@ class App extends Component {
         console.log(error)
       });
   }
-  handleChange =(e)=>{
+  handleTagChange =(e)=>{
     this.setState({
-      searchField: e.target.value
+      searchTagField: e.target.value
+    })
+  }
+  handleCaptionChange =(e)=>{
+    this.setState({
+      searchCaptionField: e.target.value
     })
   }
   render() {
-    const {tags, captions, searchField} = this.state;
-    // const filteredCaptions = captions.filter(caption=> caption.toLowerCase().includes(searchField.toLowerCase()))
-    // const filteredTags = tags.filter(tag=> tag.toLowerCase().includes(searchField.toLowerCase()))
+    const {tags, captions, searchTagField, searchCaptionField} = this.state;
+    const filteredTags = tags.filter(tag=> tag.toLowerCase().includes(searchTagField.toLowerCase()))
+    const filteredCaptions = captions.filter(caption=> caption.caption.toLowerCase().includes(searchCaptionField.toLowerCase()))
     return (
       <div className="App">
         <AppStyle>
           <div>
-            <SearchBoxContainer placeholder='Search' handleChange={this.handleChange}/>
+            <SearchBoxContainer placeholder='Filter Tags' handleChange={this.handleTagChange}/>
             <AppTitle>Tags</AppTitle>
-            {tags.length > 1 ? <TagContainer tags={tags}/> : <div>Loading tags...</div>}
+            {tags.length > 1 ? <TagContainer tags={filteredTags}/> : <div>Loading tags...</div>}
           </div>
           <div>
+          <SearchBoxContainer placeholder='Filter Captions' handleChange={this.handleCaptionChange}/>
             <AppTitle>Captions</AppTitle>
-            {tags.length > 1 ? <CaptionContainer captions={captions}/>: <div>Loading categories...</div>}
+            {tags.length > 1 ? <CaptionContainer captions={filteredCaptions}/>: <div>Loading categories...</div>}
           </div>
           {/* <SearchBox placeholder='Search Monsters' handleChange={this.handleChange}/>
           <CapList monsters={filteredMonsters}/> */}
