ea0c1c34848cd63f520cf91a6599cdd7f641b166
Ridwan Balogun <balogunridwan@gmail.com>
2020-02-25 16:06:36 +0100

    Add Component layouts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { AppStyle, AppTitle } from './styled-components/App.styles';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: []
+    }
+  }
+  componentDidMount() {
+    axios.get('https://capcards-api.herokuapp.com/v1.0/api/tag/').then(response => {
+      console.log(response.data.data.tags)
+      this.setState({ tags: response.data.data.tags });
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <AppStyle>
+          <AppTitle>{this.state.tags.map((tag, i) => <li key={i}>{tag}</li>)}</AppTitle>
+        </AppStyle>
+      </div>
+    )
+  }
+
+};
 
 export default App;
