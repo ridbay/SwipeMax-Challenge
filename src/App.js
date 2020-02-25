c1d8986b878e4974b8df761cfcb86cc8e8d25fdc
Ridwan Balogun <balogunridwan@gmail.com>
2020-02-25 17:01:35 +0100

    Fetch Endpoints

--- src/App.js
+++ src/App.js
@@ -3,30 +3,11 @@ import { AppStyle, AppTitle } from './styled-components/App.styles';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tags: [],
-      categories: []
-    }
+  state={
+    tags: [],
+    categories: [],
+    searchField: '' 
   }
-
-
-  // componentDidMount() {
-  //   axios.get('https://capcards-api.herokuapp.com/v1.0/api/tags/').then(response => {
-  //     console.log(response.data.data.tags)
-  //     this.setState({ tags: response.data.data.tags });
-  //   }).catch(error => {
-  //     console.log(error)
-  //   })
-
-  //   axios.get('https://capcards-api.herokuapp.com/v1.0/api/caption/').then(response => {
-  //     console.log(response.data.data.captions)
-  //     this.setState({ categories: response.data.data.captions });
-  //   }).catch(error => {
-  //     console.log(error)
-  //   })
-  // }
 
 
   componentDidMount() {
@@ -34,13 +15,12 @@ class App extends Component {
       axios.get('https://capcards-api.herokuapp.com/v1.0/api/tag/'),
       axios.get('https://capcards-api.herokuapp.com/v1.0/api/caption/')
     ])
-    .then(axios.spread((tag, caption) => {
-      this.setState({ tags: tag.data.data.tags });
-      this.setState({ categories: caption.data.data.captions });
-
-    })).catch(error => {
-          console.log(error)
-        });
+      .then(axios.spread((tag, caption) => {
+        this.setState({ tags: tag.data.data.tags });
+        this.setState({ categories: caption.data.data.captions });
+      })).catch(error => {
+        console.log(error)
+      });
   }
   render() {
     return (
@@ -48,12 +28,12 @@ class App extends Component {
         <AppStyle>
           <div>
             <AppTitle>Tags</AppTitle>
-            {this.state.tags.length > 1 ? <div>{this.state.tags.map((tag, i) => <li key={i}>{tag}</li>)}</div>: <div>Loading tags...</div>}
+            {this.state.tags.length > 1 ? <div>{this.state.tags.map((tag, i) => <li key={i}>{tag}</li>)}</div> : <div>Loading tags...</div>}
           </div>
           <div>
             <AppTitle>Categories</AppTitle>
             {this.state.tags.length > 1 ? <div>{this.state.categories.map((category, i) => <li key={i}>{category.caption}</li>)}</div> : <div>Loading categories...</div>}
-            
+
           </div>
 
         </AppStyle>
