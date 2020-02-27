81b47feb135f6765c186b7a41d10129beb368efe
Ridwan Balogun <balogunridwan@gmail.com>
2020-02-27 08:14:09 +0100

    Add Styles to the components

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ import SearchBoxContainer from './Components/Containers/SearchBoxContainer'
 
 class App extends Component {
   state = {
-    tags: [],
-    captions: [],
+    tags: ["golf", "sports", "food", "fish", "health", "ownership", "health", "jobs", "cars", "random"],
+    captions: [{id: 1, caption: "golf is pretty great"},{id: 2, caption: "sports get me energized"}, {id: 3, caption: "golf is the greatest sports game"}, {id: 4, caption: "who likes fish in the morning"},
+    {id: 5, caption: "i need food to get me through the day"}, {id: 6, caption: "let's make a fishy for dinner"}, {id: 7, caption: "so many jobs lately"},
+    {id: 8, caption: "just got a good job"}, {id: 9, caption: "got to check my medicine"}, {id: 10, caption: "how can that be yours"},
+    {id: 11, caption: "nothing is yours till you let it go"}, {id: 12, caption: "i love jogging daily to keep fit"}, {id: 13, caption: "oats is a good meal for kid boop"}, 
+    {id: 14, caption: "i love soccer"}, {id: 15, caption: "this is a random caption"}, {id: 16, caption: "i love to eat when i go watch the game"}, 
+    {id: 17, caption: "lagos is a good place to be."}, {id: 18, caption: "my favourite food is yam."}],
     searchTagField: '',
     searchCaptionField: '',
-    isPending: true
+    isPending: false
   }
 
 
@@ -24,7 +29,9 @@ class App extends Component {
     ])
       .then(axios.spread((tag, caption) => {
         this.setState({ tags: tag.data.data.tags });
+        console.log(tag.data.data.tags)
         this.setState({ captions: caption.data.data.captions });
+        console.log(caption.data.data.captions)
         this.setState({ isPending: false });
       })).catch(error => {
         console.log(error)
